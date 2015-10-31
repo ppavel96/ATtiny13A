e6f18a515b1a79e1510d8184450e54dd9fc0f09e
ppavel96 <papolyakov_1@edu.hse.ru>
2015-10-31 17:46:36 +0300

    fix tabs -> spaces

--- include/cmd_parser.h
+++ include/cmd_parser.h
@@ -7,43 +7,43 @@ class CmdParser;
 
 /** Command line option */
 class CmdOption {
-	std::string Name;
-	std::string Help;
-	std::string Arg;
-	bool HasArg;
+    std::string Name;
+    std::string Help;
+    std::string Arg;
+    bool HasArg;
 
 public:
     /** Creates new command line option */
-	CmdOption(const std::string &InName, const std::string &InHelp, bool InHasArg);
+    CmdOption(const std::string &InName, const std::string &InHelp, bool InHasArg);
 
-	/** Returns name */
-	const std::string& GetName() const;
+    /** Returns name */
+    const std::string& GetName() const;
 
-	/** Returns argument as string */
-	const std::string& GetArgAsString() const;
+    /** Returns argument as string */
+    const std::string& GetArgAsString() const;
 
-	/** Returns argument as int */
-	int GetArgAsInt() const;
+    /** Returns argument as int */
+    int GetArgAsInt() const;
 
-	/** Returns help */
-	const std::string& GetHelp() const;
+    /** Returns help */
+    const std::string& GetHelp() const;
 
-	friend CmdParser;
+    friend CmdParser;
 };
 
 /** Class responsible for parsing command line arguments */
 class CmdParser {
-	std::vector<CmdOption> Options;
-	std::vector<std::string> CmdArgs;
-	size_t Current;
+    std::vector<CmdOption> Options;
+    std::vector<std::string> CmdArgs;
+    size_t Current;
 
 public:
-	/** Initialize CmdParser with a set of options and ptr to argv */
-	CmdParser(const std::vector<CmdOption> &InOptions, int argc, char** argv);
+    /** Initialize CmdParser with a set of options and ptr to argv */
+    CmdParser(const std::vector<CmdOption> &InOptions, int argc, char** argv);
 
-	/** Retrieves the next option and returns true on success */
-	bool GetNext(CmdOption& OutOption);
+    /** Retrieves the next option and returns true on success */
+    bool GetNext(CmdOption& OutOption);
 
-	/** Displays help */
-	void DisplayHelp() const;
+    /** Displays help */
+    void DisplayHelp() const;
 };
--- source/cmd_parser.cpp
+++ source/cmd_parser.cpp
@@ -1,86 +1,85 @@
 #include <iostream>
-#include <iomanip>
 
 #include "cmd_parser.h"
 
 CmdOption::CmdOption(const std::string &InName, const std::string &InHelp, bool InHasArg) :
-	Name(InName), Help(InHelp), HasArg(InHasArg) {}
+    Name(InName), Help(InHelp), HasArg(InHasArg) {}
 
 const std::string& CmdOption::GetName() const {
-	return Name;
+    return Name;
 }
 
 const std::string& CmdOption::CmdOption::GetArgAsString() const {
-	return Arg;
+    return Arg;
 }
 
 int CmdOption::GetArgAsInt() const {
-	try {
-		size_t idx;
-		int res = std::stoi(Arg, &idx);
-		if (idx < Arg.size())
-			throw std::invalid_argument(Arg.c_str());
-		return res;
-	} catch (const std::invalid_argument&) {
-		std::cout << "emulator: fatal error: " << Arg << " is not a number\n";
+    try {
+        size_t idx;
+        int res = std::stoi(Arg, &idx);
+        if (idx < Arg.size())
+            throw std::invalid_argument(Arg.c_str());
+        return res;
+    } catch (const std::invalid_argument&) {
+        std::cout << "emulator: fatal error: " << Arg << " is not a number\n";
         exit(0);
-	} catch (const std::out_of_range&) {
-		std::cout << "emulator: fatal error: " << Arg << " is too big\n";
+    } catch (const std::out_of_range&) {
+        std::cout << "emulator: fatal error: " << Arg << " is too big\n";
         exit(0);
-	}
+    }
 }
 
 const std::string& CmdOption::GetHelp() const {
-	return Help;
+    return Help;
 }
 
 CmdParser::CmdParser(const std::vector<CmdOption> &InOptions, int argc, char** argv) : Options(InOptions), Current(0) {
-	for (int i = 1; i < argc; ++i)
-		CmdArgs.push_back(std::string(argv[i]));
+    for (int i = 1; i < argc; ++i)
+        CmdArgs.push_back(std::string(argv[i]));
 }
 
 bool CmdParser::GetNext(CmdOption& OutOption) {
-	if (Current == CmdArgs.size())
-		return false;
-	
-	bool Flag = false;
-	for (auto i : Options)
-		if (i.GetName() == CmdArgs[Current]) {
-			OutOption = i;
-			Flag = true;
-			break;
-		}
+    if (Current == CmdArgs.size())
+        return false;
+    
+    bool Flag = false;
+    for (auto i : Options)
+        if (i.GetName() == CmdArgs[Current]) {
+            OutOption = i;
+            Flag = true;
+            break;
+        }
 
-	if (!Flag) {
-		if (CmdArgs[Current][0] == '-') {
-			std::cout << "emulator: fatal error: unrecognized command line option " << CmdArgs[Current] << "\n";
+    if (!Flag) {
+        if (CmdArgs[Current][0] == '-') {
+            std::cout << "emulator: fatal error: unrecognized command line option " << CmdArgs[Current] << "\n";
             exit(0);
-		}
+        }
 
-		OutOption.Name = CmdArgs[Current++];
-		return true;
-	}
+        OutOption.Name = CmdArgs[Current++];
+        return true;
+    }
 
-	if (OutOption.HasArg) {
-		if (++Current == CmdArgs.size()) {
-			std::cout << "emulator: fatal error: missing argument after " << OutOption.GetName() << "\n";
+    if (OutOption.HasArg) {
+        if (++Current == CmdArgs.size()) {
+            std::cout << "emulator: fatal error: missing argument after " << OutOption.GetName() << "\n";
             exit(0);
-		}
+        }
 
-		OutOption.Arg = CmdArgs[Current];
-	}
+        OutOption.Arg = CmdArgs[Current];
+    }
 
-	++Current;
-	return true;
+    ++Current;
+    return true;
 }
 
 void CmdParser::DisplayHelp() const {
-	std::cout << "Usage: emulator [options] flash_file...\n";
-	std::cout << "Options:\n";
+    std::cout << "Usage: emulator [options] flash_file...\n";
+    std::cout << "Options:\n";
 
-	for (auto i : Options)
-		std::cout << i.GetHelp() << '\n';
-	
-	std::cout << "\n";
+    for (auto i : Options)
+        std::cout << i.GetHelp() << '\n';
+    
+    std::cout << "\n";
     std::cout << "Please note that i/o and any logs are disabled by default\n";
 }
